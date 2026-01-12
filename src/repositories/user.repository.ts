99b984ab4cac83841ffcf58986ac2644623b5609
Giselle <giselle.costa1409@hotmail.com>
2026-01-11 23:52:15 -0300

    feat: add CRUD functions to user repository

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,14 +1,48 @@
 import { prisma } from "../database/prismaClient";
 import type { CreateUserDTO } from "../schemas/user.schema";
+import { UserMapper, UserType, UserCredentialsType } from "../entities/mappers/user"; 
 
-export async function createUser(data: CreateUserDTO) {
-  return await prisma.user.create({
-    data: {
-      name: data.name, 
-      email: data.email, 
-      password: data.password,
-      role: data.role,
-    },
-    select: { id: true, name: true, email: true, role: true },
+// Create new user
+export async function createUser(data: CreateUserDTO): Promise<UserType> {
+  const user = await prisma.user.create({
+    data,
   });
+  return UserMapper.mapSchemaToEntity(user);
 }
+
+// Read user
+export async function getUserById(id: string): Promise<UserType | null> {
+  const user = await prisma.user.findUnique({
+    where: { id},
+  });
+  if (!user) {
+    return null;
+  }
+  return UserMapper.mapSchemaToEntity(user);
+}
+
+// Update user by ID
+export async function updateUser(id: string, data: Partial<CreateUserDTO>): Promise<UserType | null> {
+  const user = await prisma.user.update({
+    where: { id },
+    data
+  });
+  return UserMapper.mapSchemaToEntity(user);
+}
+
+// Delete user by ID
+export async function deleteUser(id: string): Promise<boolean> {
+  const deleted = await prisma.user.delete({
+    where: { id },
+  });
+  return !!deleted;
+}
+
+
+
+
+
+
+
+
+
