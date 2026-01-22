5a45fba05c3dde91d769303dcbe6904f0ff1b91e
Giselle <giselle.costa1409@hotmail.com>
2026-01-22 15:14:40 -0300

    fix: update prisma configuration files

--- src/database/prismaClient.ts
+++ src/database/prismaClient.ts
@@ -1,6 +1,10 @@
-import pkg from "@prisma/client";
-const { PrismaClient } = pkg;
+import "dotenv/config";
+import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaClient } from '../generated/prisma/client'
 
-export const prisma = new PrismaClient();
+const connectionString = `${process.env.DATABASE_URL}`
 
+const adapter = new PrismaPg({ connectionString })
+const prisma = new PrismaClient({ adapter })
 
+export { prisma }
