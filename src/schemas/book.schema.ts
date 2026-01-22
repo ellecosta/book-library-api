02aacddeb3642ed041cf208f78281e7475130501
Giselle <giselle.costa1409@hotmail.com>
2026-01-22 15:20:32 -0300

    feat: add book and booking schemas

--- src/schemas/book.schema.ts
+++ src/schemas/book.schema.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const CreateBookSchema = z.object({
-    title: z.string().trim().min(2, "Title must have at least 2 characters"),
-    author: z.string().trim().min(2, "Author must have at least 2 characters"),
-    avaliable: z.boolean().default(true)
+    title: z.string(),
+    author: z.string(),
+    avaliable: z.boolean()
 });
--- src/schemas/booking.schema.ts
+++ src/schemas/booking.schema.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
 export const CreateBookingSchema = z.object({
-    userId: z.uuid("Invalid userId"),
-    bookId: z.uuid("Invalid bookId")
+    userId: z.uuid(),
+    bookId: z.uuid(),
 });
